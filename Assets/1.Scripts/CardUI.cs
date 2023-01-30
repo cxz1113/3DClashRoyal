73e746505abc6573279a1cf67dd21d9401294c06
1JeongYeon <98639630+cxz1113@users.noreply.github.com>
2023-01-30 21:54:57 +0900

    Modify#15

--- Assets/1.Scripts/CardUI.cs
+++ Assets/1.Scripts/CardUI.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.EventSystems;
 
-public class CardUI : MonoBehaviour
+public class CardUI : MonoBehaviour, IDragHandler, IDropHandler 
 {
     [SerializeField] private Button btn;
     [SerializeField] private GameObject cardBackGround;
     [SerializeField] private Transform parent;
     [SerializeField] private TMP_Text costText;
     [SerializeField] private Image image;
+    [SerializeField] private Canvas canvas;
+
     CardData cardData;
+    RaycastHit hit;
     public int Cost { get; set; }
     public bool Empty { get; set; }
 
@@ -40,12 +44,16 @@ public class CardUI : MonoBehaviour
 
     void Update()
     {
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 100))
+        {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.red);
+        }
         costText.text = Cost.ToString();
     }
     void OnButtonSpawn()
     {
         // 카드 생성 및 몬스터 생성
-        if(ControllerManager.Instance.uiCont.IsEnergyCheck(cardData.Cost) == true)
+        /*if(ControllerManager.Instance.uiCont.IsEnergyCheck(cardData.Cost) == true)
         {
             ControllerManager.Instance.uiCont.Energy -= cardData.Cost;
             Character character = Instantiate(cardData.Cha, parent);
@@ -55,7 +63,7 @@ public class CardUI : MonoBehaviour
             Enable(false);
             Empty = true;           
         }        
-        ControllerManager.Instance.cardCont.Invoke("AddCard", 1f);
+        ControllerManager.Instance.cardCont.Invoke("AddCard", 1f);*/
     }
 
 
@@ -78,5 +86,38 @@ public class CardUI : MonoBehaviour
         Cost = cardData.Cost;
         Empty = false;
         return this;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        GetComponent<RectTransform>().anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if(ControllerManager.Instance.uiCont.IsEnergyCheck(cardData.Cost)  == true)
+        {
+            if(hit.transform.tag.Equals("ray"))
+            {
+                
+                ControllerManager.Instance.uiCont.Energy -= cardData.Cost;
+                Character character = Instantiate(cardData.Cha, parent);
+                character.cardData = cardData;
+                character.tag = "my";
+                character.charData.findTag = "enemy";
+                character.transform.position = new Vector3(hit.point.x, 0.5f, hit.point.z);
+                Enable(false);
+                Empty = true;
+                ControllerManager.Instance.cardCont.Invoke("AddCard", 1f);
+            }
+        }
+        StartCoroutine("CardPosInit");
+    }
+
+    IEnumerator CardPosInit()
+    {
+        transform.parent.GetComponent<HorizontalLayoutGroup>().enabled = false;
+        yield return new WaitForSeconds(0.1f);
+        transform.parent.GetComponent<HorizontalLayoutGroup>().enabled = true;
     }
 }
