27cad28e7c624a0522f7b2aa20b5ca9298d55cab
1JeongYeon <98639630+cxz1113@users.noreply.github.com>
2023-02-02 21:58:59 +0900

    Modify#18

--- Assets/1.Scripts/House/Castle.cs
+++ Assets/1.Scripts/House/Castle.cs
@@ -10,12 +10,7 @@ public class Castle : MonoBehaviour
     private float curHP;
 
     [SerializeField] private Image hpImage;
-    [SerializeField] private GameObject prefab;
-    [SerializeField] private Transform parent;
-
-    float attackDelay = 3f;
-    float attacTime = 0;
-    Character character;
+    
     
     public float HP
     {
@@ -38,43 +33,5 @@ public class Castle : MonoBehaviour
     void Start()
     {
         curHP = maxHp;
-    }
-
-    void Update()
-    {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag(character.charData.findTag);
-        if (objs.Length == 0)
-            return;
-        float distance = 10f;
-        GameObject target = null;
-        foreach(var obj in objs)
-        {
-            float dis = Vector3.Distance(parent.transform.position, obj.transform.position);
-            target = obj;
-            distance = dis;
-        }
-        float targetPosition = Vector3.Distance(parent.transform.position, target.transform.position);
-        if (target == null)
-            return;
-        if(targetPosition < distance)
-        {
-            parent.transform.LookAt(target.transform);
-            Attack();
-        }
-    }
-
-    void Attack()
-    {
-        attacTime += Time.deltaTime;
-        if(attacTime > attackDelay)
-        {
-            attacTime = 0;
-            StartCoroutine("BulletCreat");
-        }
-    }
-    IEnumerator BulletCreat()
-    {
-        GameObject bullet = Instantiate(prefab, parent);
-        yield return new WaitForSeconds(1f);
     }
 }
---
+++ Assets/1.Scripts/Turret.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private Transform parent;
+
+    float attackDelay = 3f;
+    float attacTime = 0;
+    Character character;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //FindEnemy();
+    }
+
+    void FindEnemy()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag(character.charData.findTag);
+        if (objs.Length == 0)
+            return;
+        float distance = 10f;
+        GameObject target = null;
+        foreach (var obj in objs)
+        {
+            float dis = Vector3.Distance(parent.transform.position, obj.transform.position);
+            target = obj;
+            distance = dis;
+        }
+        float targetPosition = Vector3.Distance(parent.transform.position, target.transform.position);
+        if (target == null)
+            return;
+        if (targetPosition < distance)
+        {
+            parent.transform.LookAt(target.transform);
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        attacTime += Time.deltaTime;
+        if (attacTime > attackDelay)
+        {
+            attacTime = 0;
+            StartCoroutine("BulletCreat");
+        }
+    }
+    IEnumerator BulletCreat()
+    {
+        GameObject bullet = Instantiate(prefab, parent);
+        yield return new WaitForSeconds(1f);
+    }
+}
